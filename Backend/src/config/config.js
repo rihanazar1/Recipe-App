f892211fd8cdba09a33d8a64895fb762dc07139e
rihanazar1 <rihanazhar5@gmail.com>
2025-06-28 16:45:29 +0530

    remove config from github

--- Backend/src/config/config.js
+++
@@ -1,9 +0,0 @@
-import {config} from "dotenv"
-config()
-
-const _config = {
-    PORT : process.env.PORT || 4500,
-    MONGO_URI : process.env.MONGO_URI || "mongodb+srv://rihanazar0:O3foRJNH1cKTvSsv@cluster1.kawjq.mongodb.net/recipe-app"
-}
-
-export default Object.freeze(_config);
